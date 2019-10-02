7f3ad616024c856c8c243a0b68f9ce3898603c5e
Yoandy Vargas <Yoandy.Vargas@icloud.com>
2019-10-02 13:34:49 -0400

    Finalized Lab configuration
    Practice using components and import/export alongside JSX HTML

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/index.js';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Card content="This is a card!"></Card>
     </div>
   );
 }
