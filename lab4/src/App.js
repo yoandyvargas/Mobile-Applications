efb12b075ad7d9162193ab2a55f29e4005c2bc7e
Yoandy Vargas <Yoandy.Vargas@icloud.com>
2019-10-17 14:51:31 -0400

    Removed Default Logo and Text

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -6,20 +6,7 @@ import Card from './components/Card/index.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
       <Card content="This is a card!"></Card> 
     </div>
   );
