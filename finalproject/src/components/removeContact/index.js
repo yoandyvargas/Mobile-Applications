7506a6b10e1a186812de7f073f6bbd9475ea54fa
Yoandy Vargas <Yoandy.Vargas@icloud.com>
2019-12-08 01:45:13 -0500

    removeContact

---
+++ finalproject/src/components/removeContact/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class RemoveContact extends React.Component {
+
+
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default RemoveContact;
